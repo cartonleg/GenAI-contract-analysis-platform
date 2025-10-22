e8d2e16848749e4ee82ae7ed815336719d0dce1a
ammar <momaniammar95@gmail.com>
2025-10-22 19:48:36 +0300

    moved authentication to the auth dependency

--- src/controllers/GenAIController.py
+++ src/controllers/GenAIController.py
@@ -4,18 +4,9 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from services.GenAIService import GenAIService
 from services.SecurityService import SecurityService
 from .schemes.EvaluateRequest import EvaluateRequest
+from dependencies.auth import verify_jwt
 
 genai_router = APIRouter(prefix="/genai")
-security = HTTPBearer()
-
-async def verify_jwt(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    security_service = SecurityService()
-    application_user_data = security_service.verify_access_token(credentials.credentials)
-
-    if not application_user_data:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
-    
-    return application_user_data
 
 @genai_router.post("/analyze-contract")
 async def analyze_contract(file: UploadFile = File(...), user_data: dict = Depends(verify_jwt)):
